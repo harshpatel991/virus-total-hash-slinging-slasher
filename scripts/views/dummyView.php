8d08262b61fca345a8066ec0cb0c8051d536a469
pianoajd <pianoajdunk@gmail.com>
2015-03-19 19:23:16 -0500

    Added api key.

--- scripts/views/dummyView.php
+++ scripts/views/dummyView.php
@@ -8,6 +8,6 @@
 
 require_once(dirname(__FILE__).'/../VirusTotalApiV2.php');
 
-$virusTotalAPI = new VirusTotalAPIV2('a');
+$virusTotalAPI = new VirusTotalAPIV2('a503e42713758ee85a14e3afe2632db3dc92ddcf5d83e79aa985704c088a312c');
 print_r( $virusTotalAPI->getFileReport('a854cb1793501645398b872a56ce0da9bc6575be9d033080cc51c6ec9c93efd6'));
 
